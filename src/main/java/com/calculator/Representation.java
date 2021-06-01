0969b4d9db64fc0ae33eb116c60a1129efe3a02d
Patrik Nygren <patriknygren82@gmail.com>
2021-06-01 13:10:51 +0200

    Added code for running calculator

---
+++ src/main/java/com/calculator/Representation.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class Representation {
+    double num1;
+    double num2;
+    char operator;
+
+    public Representation(double num1, double num2, char operator) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operator = operator;
+    }
+
+}
